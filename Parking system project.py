fdb15dbc38cfa25a6acd6a601dfc5a45d2004368
Moises Lima Martinez <moises.lm1@outlook.com>
2026-01-30 23:23:39 -0800

    Testing harcode

--- Parking system project.py
+++ Parking system project.py
@@ -1,2 +1,24 @@
-
+#This tool called Flask is an open source micro web framwork, was suggested by ChatGPT to create a web applicatiuon and API's
+#What it provides Core functionality like URL routing and handling requests, leaves decisions about tools for databases, from validation, and authentication to development 
+#Built on top of other powerful libraries: Werkzeug (Web server gateway interface), Jinja(powerful templating engine, used to embed python logic and dynamic data into HTYML files),
+#and click(Creating flasks command line interface trools)
     
+from flask import Flask, jsonify
+app = Flask (__name__)
+
+#Hardcoded data representing parking spots
+parking_spots = [
+    {"id":1, "occupied": False},
+    {"id":2, "occupied": True},
+    {"id":3, "occupied": False},
+]
+
+@app.route("/spots")
+def get_spots():
+    return jsonify(parking_spots)
+
+@app.route("/")
+def home():
+    return "Welcome to QuickSpot Parking System"
+if __name__ == "__main__":
+    app.run(debug=True)  #debug=True enables auto-reloading and better error messages
