54b2663a0e4e5dd98e3389e0e6dbbb9d6fdb017d
Moises Lima Martinez <moises.lm1@outlook.com>
2026-02-02 19:58:58 -0800

    added lists

--- Parking system project.py
+++ Parking system project.py
@@ -23,14 +23,34 @@ def home():
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000,debug=True)  #debug=True enables auto-reloading and better error messages
     
-    #variables
-    #parking lot
-    lot_id = None
-    lot_name = None
-    lot_updated = None
-    total_spots = None
-    #parking spots
-    spot_id = None
-    lot_id
-    occupied = None
-    open_spots = None
+#variables
+#parking lot
+lot_id = 1
+lot_name = "Cerritos Mall"
+lot_updated = None
+total_spots = 10
+#parking spots
+spot_id = None
+lot_id
+occupied_count = 0 
+open_spots = None
+#list of parking spots in the lot
+spots = [
+    {"spot_id": 1, "occupied": False},
+    {"spot_id": 2, "occupied": True},
+    {"spot_id": 3, "occupied": False},
+    {"spot_id": 4, "occupied": False},
+    {"spot_id": 5, "occupied": True},
+    {"spot_id": 6, "occupied": False},
+    {"spot_id": 7, "occupied": False},
+    {"spot_id": 8, "occupied": True},
+    {"spot_id": 9, "occupied": False},
+    {"spot_id": 10, "occupied": False},
+]
+#checks which spots are occupied and adds it to the count 
+for spot in spots:
+    if spot["occupied"]:
+        occupied_count += 1
+print("Occupied spots:", occupied_count)
+open_spots = total_spots - occupied_count
+print("Open spots:", open_spots)
