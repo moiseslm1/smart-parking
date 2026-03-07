1d7cb23ebe37fc813231dea75851a0d8d4117232
Moises Lima Martinez <moises.lm1@outlook.com>
2026-03-06 22:46:19 -0800

    Continue Vehicle class

--- vehicle.py
+++ vehicle.py
@@ -1,13 +1,24 @@
+import random
+import string 
 from datetime import datetime 
 class Vehicle:
-    def __init__(self, plate_number):
-        self.plate_number = plate_number
+    def __init__(self, plate_number=None):
+        if plate_number is None:
+            self.plate_number = self.generate_plate()
+        else:
+            self.plate_number = plate_number
+
         self.entry_time = datetime.now()
+        self.exit_time = None
+
+    @staticmethod
+    def generate_plate():
+        letters = '',join(random.choices(string.ascii_uppercase, k=3))
+        numbers = '',join(random.choices(string.digits, k=3))
+        return f"{letters}-{numbers}"
+    
+    def leave(self):
+        self.exit_time = datetime.now()
+    
     def to_dict(self):
-        return {
-            "plate_number": self.plate_number,
-            "entry_time": self.entry_time.isoformat()
-        }
         
-    def __str__(self):        
-        return self.plate_number
